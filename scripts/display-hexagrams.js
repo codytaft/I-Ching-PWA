ece12ac8eca8a30fdf1b04080260172be41b88a7
Cody Taft <cody.taft@gmail.com>
2019-02-03 12:34:38 -0700

    Refactor display-hexagrams

--- scripts/display-hexagrams.js
+++ scripts/display-hexagrams.js
@@ -1,11 +1,15 @@
+let hexagram = [];
+let changesdHexagram = [];
+let changedHexagram = [];
+let noChange = true
+
 $('.hexagrams').hide()
+
 $('.cast-btn').on('click', (e) => {
   $('.cast-btn').attr('disabled', 'disabled')
-  $('.hexagrams').hide().show()
-  let hexagram = [];
+  $('.hexagrams').show()
   for (let i = 0; i < 6; i++) {
     lineCast();
-    $(hexagramLine).addClass("pos" + (i + 1)).hide().prependTo('.hexagram').fadeIn(3000);
     hexagram.push(hexagramLine)
   }
   changeHexagram(hexagram)
@@ -13,26 +17,35 @@ $('.cast-btn').on('click', (e) => {
 });
 
 changeHexagram = (hexagram) => {
-  let changedHexagram = [];
-  let noChange = true
   hexagram.forEach((line, i) => {
     let changedLine = line.replace(/old-yin/gi, 'young-yang').replace(/old-yang/gi, 'young-yin')
     changedHexagram.push(changedLine)
 
+    // DETECT CHANGE IN HEXAGRAM
     if (hexagram[i] != changedHexagram[i]) {
       noChange = false
     }
   })
 
+  // DISPLAY HEXAGRAM OR HEXAGRAMS
   if (!noChange) {
-    addChangedHexagram(changedHexagram)
+    displayHexagrams(hexagram, changedHexagram)
+  } else {
+    displayHexagrams(hexagram)
   }
 }
 
-addChangedHexagram = (changedHexagram) => {
-  $('.arrows').hide().fadeIn(3000)
-  $('.changedHexagram').toggle()
-  changedHexagram.forEach((line, i) => {
-    $(line).addClass("pos" + (i + 1)).hide().prependTo('.changedHexagram').fadeIn(3000);
+displayHexagrams = (hexagram, changedHexagram) => {
+  hexagram.forEach((line, i) => {
+    $(line).addClass("pos" + (i + 1)).hide().prependTo('.hexagram').fadeIn(3000);
   })
+
+  if (changedHexagram) {
+    $('.changedHexagram').toggle()
+    $('.arrows').hide().fadeIn(3000)
+    changedHexagram.forEach((line, i) => {
+      $(line).addClass("pos" + (i + 1)).hide().prependTo('.changedHexagram').fadeIn(3000);
+    })
+  }
 }
+
